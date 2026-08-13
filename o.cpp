cb7a6351465e69f5339a9bbfa53367ccfc7d2ecf
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-14 01:53:22 +0530

    add : s1 o

---
+++ o.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a , b;
+  char s;
+  cin>>a>>s>>b;
+
+  if(s=='+'){
+    cout<<a+b<<'\n';
+  }
+  else if(s=='-'){
+    cout<<a-b<<'\n';
+  }
+  else if(s=='*'){
+    cout<<a*b<<'\n';
+  }
+  else if(s=='/'){
+    cout<<a/b<<'\n';
+  }
+  return 0;
+}
