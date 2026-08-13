f97ee01d314b229c5881f9224849efc01f96b72c
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-08-13 19:00:20 +0530

    add : s1 b

---
+++ sheet1/b.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int integer;
+  long long longer;
+  char character;
+  float floating;
+  double doubles;
+
+  cin>>integer >> longer >> character >> floating >> doubles;
+
+  cout<<integer<<'\n' << longer<<'\n' << character <<'\n'<< floating<<'\n' << doubles<<'\n';
+  return 0;
+}
