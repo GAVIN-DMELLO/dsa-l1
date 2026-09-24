cf6f6cef48cb442215c08a73d6304bde1b22a2cb
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-24 23:36:42 +0530

    add : char a to e 5 times vertically

---
+++ strike/1-to-5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  // for(int i=0 ; i<6 ; i++){
+  //   for(int j=1 ; j<=5 ; j++){
+  //     cout<<j<<" ";
+  //   }
+  //   cout<<'\n';
+  // }
+
+  for(int j=1 ; j<=5 ; j++){
+    for(char i='a' ; i<='e' ; i++){
+      cout<<i<<" ";
+    } 
+    cout<<endl;
+  }
+
+  return 0;
+}
