707d5373b7c67685e9448593e7d9e6b15bea2739
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-24 23:40:01 +0530

    add : 10 to 1

--- strike/1-to-5.cpp
+++ strike/1-to-5.cpp
@@ -10,11 +10,16 @@ int main(){
   //   cout<<'\n';
   // }
 
-  for(int j=1 ; j<=5 ; j++){
-    for(char i='a' ; i<='e' ; i++){
-      cout<<i<<" ";
-    } 
-    cout<<endl;
+  // for(int j=1 ; j<=5 ; j++){
+  //   for(char i='a' ; i<='e' ; i++){
+  //     cout<<i<<" ";
+  //   } 
+  //   cout<<endl;
+  // }
+
+
+  for(int i=10;i>=0;i--){
+    cout<<i<<" ";
   }
 
   return 0;
