a9c85cb6b9bdf74691498dfbbc7da41b98302a13
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-24 21:27:53 +0530

    add : positive negative or zero

---
+++ strike/pos-neg-or-zero.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int n;
+  cin>>n;
+
+  if(n>0){
+    cout<<n <<" is positive"<<'\n';
+  }else if(n<0){
+    cout<<n <<" is negative"<<'\n';
+  }else{
+    cout<<n <<" is zero"<<'\n';
+  }
+
+  return 0;
+}
