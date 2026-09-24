762f1abca63d966b57372a1f363304e55a5bec77
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-24 21:32:47 +0530

    add : calculator

---
+++ strike/calculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int a , b;
+  char op;
+
+  cin>>a>>op>>b;
+
+  if(op == '+'){
+    cout<<a+b<<'\n';
+  }else if(op == '-'){
+    cout<<a-b<<'\n';
+  }else if(op == '*'){
+    cout<<a*b<<'\n';
+  }else if(op == '/'){
+    cout<<a/b<<'\n';
+  }else if(op == '%'){
+    cout<<a%b <<'\n';
+  }
+
+  return 0;
+}
