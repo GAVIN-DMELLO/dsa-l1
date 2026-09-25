1ea16542146a7f2264c168d094cd7f588e8712a1
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-25 15:49:24 +0530

    add : reverse star pattern

--- strike/1-to-5.cpp
+++ strike/1-to-5.cpp
@@ -50,44 +50,89 @@ int main(){
 
 
 
-  for(int i=1;i<=1;i++){
+  // for(int i=1;i<=1;i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1;i<=2;i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1;i<=3;i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1;i<=4;i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1;i<=5;i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+
+  // for(int j=1 ; j<=5 ; j++){
+  //   for(int i=1;i<=j;i++){
+  //     cout<<"*";
+  //   }
+
+  //   cout<<endl;
+  // }
+
+
+
+  for(int i=1 ; i<=5 ; i++){
     cout<<"*";
   }
 
   cout<<endl;
 
-  for(int i=1;i<=2;i++){
+  for(int i=1 ; i<=4 ; i++){
     cout<<"*";
   }
 
   cout<<endl;
 
-  for(int i=1;i<=3;i++){
+  for(int i=1 ; i<=3 ; i++){
     cout<<"*";
   }
 
   cout<<endl;
 
-  for(int i=1;i<=4;i++){
+  for(int i=1 ; i<=2 ; i++){
     cout<<"*";
   }
 
   cout<<endl;
 
-  for(int i=1;i<=5;i++){
+  for(int i=1 ; i<=1 ; i++){
     cout<<"*";
   }
 
   cout<<endl;
 
 
-  for(int j=1 ; j<=5 ; j++){
-    for(int i=1;i<=j;i++){
+
+  for(int j=5 ; j>=1 ; j--){
+    for(int i=1 ; i<=j ; i++){
       cout<<"*";
     }
 
     cout<<endl;
   }
+
+
+
 
 
 
