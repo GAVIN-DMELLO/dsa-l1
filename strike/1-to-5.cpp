5ec6d899a683ee9309a995bed7dd7c45e6d93cc4
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-25 15:43:51 +0530

    add : star pattern breakdown

--- strike/1-to-5.cpp
+++ strike/1-to-5.cpp
@@ -30,12 +30,70 @@ int main(){
   // }
 
 
-  for(int i=1 ; i<=7 ; i++){
-    for(int j=1 ; j<=5 ; j++){
+  // for(int i=1 ; i<=7 ; i++){
+  //   for(int j=1 ; j<=5 ; j++){
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+  // }
+
+
+  // for(int i=1 ; i<=5 ; i++){
+  //   for(int j=1 ; j<=5 ; j++){
+  //     cout<<"*";
+  //     if(j == i){
+  //       break;
+  //     }
+  //   }
+  //   cout<<endl;
+  // }
+
+
+
+  for(int i=1;i<=1;i++){
+    cout<<"*";
+  }
+
+  cout<<endl;
+
+  for(int i=1;i<=2;i++){
+    cout<<"*";
+  }
+
+  cout<<endl;
+
+  for(int i=1;i<=3;i++){
+    cout<<"*";
+  }
+
+  cout<<endl;
+
+  for(int i=1;i<=4;i++){
+    cout<<"*";
+  }
+
+  cout<<endl;
+
+  for(int i=1;i<=5;i++){
+    cout<<"*";
+  }
+
+  cout<<endl;
+
+
+  for(int j=1 ; j<=5 ; j++){
+    for(int i=1;i<=j;i++){
       cout<<"*";
     }
+
     cout<<endl;
   }
+
+
+
+  
+
+
 
   return 0;
 }
