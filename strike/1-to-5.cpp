409201e4435426de9b03b383cdebae10e2f66244
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-25 18:54:57 +0530

    add : inverted right triangle

--- strike/1-to-5.cpp
+++ strike/1-to-5.cpp
@@ -91,46 +91,60 @@ int main(){
 
 
 
-  for(int i=1 ; i<=5 ; i++){
+  // for(int i=1 ; i<=5 ; i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1 ; i<=4 ; i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1 ; i<=3 ; i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1 ; i<=2 ; i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+  // for(int i=1 ; i<=1 ; i++){
+  //   cout<<"*";
+  // }
+
+  // cout<<endl;
+
+
+
+  // for(int j=5 ; j>=1 ; j--){
+  //   for(int i=1 ; i<=j ; i++){
+  //     cout<<"*";
+  //   }
+
+  //   cout<<endl;
+  // }
+
+
+
+for(int k=1 ; k<=5 ; k++){
+  for(int i=1 ; i<=5-k ; i++){
+    cout<<" ";
+  }
+  for(int j=1 ; j<=k ; j++){
     cout<<"*";
   }
-
-  cout<<endl;
-
-  for(int i=1 ; i<=4 ; i++){
-    cout<<"*";
-  }
-
-  cout<<endl;
-
-  for(int i=1 ; i<=3 ; i++){
-    cout<<"*";
-  }
-
-  cout<<endl;
-
-  for(int i=1 ; i<=2 ; i++){
-    cout<<"*";
-  }
-
-  cout<<endl;
-
-  for(int i=1 ; i<=1 ; i++){
-    cout<<"*";
-  }
-
   cout<<endl;
 
 
-
-  for(int j=5 ; j>=1 ; j--){
-    for(int i=1 ; i<=j ; i++){
-      cout<<"*";
-    }
-
-    cout<<endl;
-  }
-
+}
+  
 
 
 
