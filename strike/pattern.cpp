87b6f98ec266b9c50c355ee678e798b32b0b3e45
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-09-25 22:22:35 +0530

    add : inverted pyramid

--- strike/pattern.cpp
+++ strike/pattern.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main(){
-  for(int k=5 ; k>=1 ; k--){
-    for(int i=1 ; i<=k ; i++){
-      cout<<" ";
-    }
-    for(int j=1;j<=(11-2*k);j++){
-      cout<<"*";
-    }
-    for(int i=1;i<=k;i++){
-      cout<<" ";
-    }
-    cout<<endl;
-  }
+  // for(int k=5 ; k>=1 ; k--){
+  //   for(int i=1 ; i<=k ; i++){
+  //     cout<<" ";
+  //   }
+  //   for(int j=1;j<=(11-2*k);j++){
+  //     cout<<"*";
+  //   }
+  //   for(int i=1;i<=k;i++){
+  //     cout<<" ";
+  //   }
+  //   cout<<endl;
+  // }
   
 
 
@@ -66,6 +66,68 @@ int main(){
   // cout<<endl;
 
 
+  for(int k=0 ; k<=4 ; k++){
+    for(int j=1 ; j<=k ; j++){
+      cout<<" ";
+    }
+    for(int i=1 ; i<=(9-2*k) ; i++){
+      cout<<"*";
+    }
+    for(int j=1 ; j<=k ; j++){
+      cout<<" ";
+    }
+    cout<<endl;
+  }
+  
+
+
+  // for(int i=1 ; i<=1 ; i++){
+  //   cout<<" ";
+  // }
+  // for(int j=1 ; j<=7 ; j++){
+  //   cout<<"*";
+  // }
+  // for(int i=1 ; i<=1 ; i++){
+  //   cout<<" ";
+  // }
+  // cout<<endl;
+
+
+  // for(int i=1 ; i<=2 ; i++){
+  //   cout<<" ";
+  // }
+  // for(int j=1 ; j<=5 ; j++){
+  //   cout<<"*";
+  // }
+  // for(int i=1 ; i<=2 ; i++){
+  //   cout<<" ";
+  // }
+  // cout<<endl;
+
+
+  // for(int i=1 ; i<=3 ; i++){
+  //   cout<<" ";
+  // }
+  // for(int j=1 ; j<=3 ; j++){
+  //   cout<<"*";
+  // }
+  // for(int i=1 ; i<=3 ; i++){
+  //   cout<<" ";
+  // }
+  // cout<<endl;
+
+
+
+  // for(int i=1 ; i<=4 ; i++){
+  //   cout<<" ";
+  // }
+  // for(int j=1 ; j<=1 ; j++){
+  //   cout<<"*";
+  // }
+  // for(int i=1 ; i<=4 ; i++){
+  //   cout<<" ";
+  // }
+  // cout<<endl;
 
   return 0;
 }
